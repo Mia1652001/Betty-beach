54ffbd63d4e97ff0fc782665e266ce035a0cf2ea
mia davidson <miadavidson@mia-sin-MacBook-Pro.local>
2026-05-31 16:14:52 +0400

    remove product section from landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AnnouncementBar from "./components/AnnouncementBar";
 import Navbar from "./components/Navbar";
 import EditorialBanner from "./components/EditorialBanner";
 import CategoryMosaic from "./components/CategoryMosaic";
-import FeaturedProducts from "./components/FeaturedProducts";
 import Footer from "./components/Footer";
 
 export default function Home() {
@@ -12,8 +11,7 @@ export default function Home() {
       <Navbar />
       <EditorialBanner />
 <CategoryMosaic />
-      <FeaturedProducts />
-      <Footer />
+<Footer />
     </main>
   );
 }
