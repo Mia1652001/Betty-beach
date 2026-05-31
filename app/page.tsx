282e8d61fe3cba7876f57c298e7111857e785810
mia davidson <miadavidson@mia-sin-MacBook-Pro.local>
2026-05-31 13:48:12 +0400

    remove As Seen In section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AnnouncementBar from "./components/AnnouncementBar";
 import Navbar from "./components/Navbar";
 import EditorialBanner from "./components/EditorialBanner";
-import AsSeenOn from "./components/AsSeenOn";
 import CategoryMosaic from "./components/CategoryMosaic";
 import FeaturedProducts from "./components/FeaturedProducts";
 import Footer from "./components/Footer";
@@ -12,8 +11,7 @@ export default function Home() {
       <AnnouncementBar />
       <Navbar />
       <EditorialBanner />
-      <AsSeenOn />
-      <CategoryMosaic />
+<CategoryMosaic />
       <FeaturedProducts />
       <Footer />
     </main>
