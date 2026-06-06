38b8897c16a8f7bfb03e24957be60adf584f8469
Mia1652001 <mia1652001@gmail.com>
2026-06-06 14:57:05 +0400

    update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -95,7 +95,7 @@ export default function Footer() {
 
           {/* Right — logo */}
           <div className="flex md:justify-end" style={{ color: "var(--text)", paddingRight: "60px" }}>
-            <BettyBeachStudiosLogo style={{ height: "120px" }} />
+            <BettyBeachStudiosLogo style={{ height: "150px" }} />
           </div>
         </div>
       </div>
