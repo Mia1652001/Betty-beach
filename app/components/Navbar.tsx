53e21e343fbc5069002a65dfee98bd78b3a1834b
Mia1652001 <mia1652001@gmail.com>
2026-06-10 12:21:02 +0400

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -222,7 +222,7 @@ export default function Navbar() {
               onClick={() => setDrawerOpen(false)}
               className="hover:opacity-60 transition-opacity duration-200"
             >
-              <BettyBeachLogo color="black" style={{ height: "32px", width: "auto" }} />
+              <BettyBeachLogo color="black" style={{ height: "42px", width: "auto", marginTop: "8px" }} />
             </Link>
             <button
               onClick={() => setDrawerOpen(false)}
